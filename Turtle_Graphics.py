fc36ada9c236a47c1c710d5404299209497620d2
MatchaCat26 <lawprose.fiona@gmail.com>
2025-11-17 18:00:59 -0500

    Created the turt the screen/ starter code and made names of functions

---
+++ Turtle_Graphics.py
@@ -0,0 +1,28 @@
+import turtle
+
+screen=turtle.Screen()
+screen.title("Ze FLYING TURTLE HAS ARRIVED")
+screen.bgcolor("white")
+
+t=turtle.Turtle()
+t.shape("turtle")
+t.color("green")
+t.speed(3)
+t.pensize(2)
+
+def move_forward():
+
+def move_backward():
+
+def turn_left():
+
+def turn_right():
+
+
+pen_down=True
+
+def toggle_pen():
+
+def clear_and_reset():
+
+turtle.done()
