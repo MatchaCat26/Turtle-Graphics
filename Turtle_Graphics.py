ca607cee8e476289b6ec407407b5f3b95f8ebb0a
MatchaCat26 <lawprose.fiona@gmail.com>
2025-11-24 17:18:56 -0500

    We finished. I MADE A HAT!

--- Turtle_Graphics.py
+++ Turtle_Graphics.py
@@ -11,18 +11,42 @@ t.speed(3)
 t.pensize(2)
 
 def move_forward():
+    t.forward(20)
 
 def move_backward():
+    t.backward(20)
 
 def turn_left():
+    t.left(15)
 
 def turn_right():
+    t.right(15)
 
 
 pen_down=True
 
 def toggle_pen():
+    global pen_down
+    if pen_down:
+        t.penup()
+        pen_down = False
+    else:
+        t.pendown()
+        pen_down = True
 
 def clear_and_reset():
+    t.clear()
+    t.penup()
+    t.goto(0,0)
+    t.setheading(0)
+    t.pendown()
+
+screen.listen()
+screen.onkey(move_forward,"Up")
+screen.onkey(move_backward,"Down")
+screen.onkey(turn_left,"Left")
+screen.onkey(turn_right,"Right")
+screen.onkey(toggle_pen,"space")
+screen.onkey(clear_and_reset,"c")
 
 turtle.done()
